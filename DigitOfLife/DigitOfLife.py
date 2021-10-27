97e95f300a8150976bdfa6dda4f6e5dcb2af769e
Lindo Gamede <lindohgamede@gmail.com>
2021-10-27 12:31:01 +0200

    First commit

--- DigitOfLife/DigitOfLife.py
+++ DigitOfLife/DigitOfLife.py
@@ -1,1 +1,20 @@
+#Ask the user for their date of birth
+dob = input('Please enter date of birth in the \'YYYYMMDD\' format: ')
 
+#Digit of Life
+temp = 0
+DOL = 0
+
+#Calculate the Digit of Life
+for ch in dob:
+    temp += int(ch)
+
+if temp > 9:
+    while temp > 9:
+        for digit in str(temp):
+            DOL += int(digit)
+        temp = DOL
+            
+if temp <= 9:
+    DOL = temp
+    print('Digit of Life: ', DOL)
